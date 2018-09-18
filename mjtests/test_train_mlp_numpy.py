06595495cda8823898ba6e0d205bf58cdac8182c
maartje <maartjedejonge@gmail.com>
2018-09-18 10:02:53 +0200

    clean

--- mjtests/test_train_mlp_numpy.py
+++ mjtests/test_train_mlp_numpy.py
@@ -1,24 +1,25 @@
 import unittest
-from mjcode.train_mlp_numpy import accuracy
+import mjcode.train_mlp_numpy 
+from mjcode.train_mlp_numpy import accuracy, FLAGS
 import numpy as np
 
 class TestTrainMLPNumpy(unittest.TestCase):
 
   def test_accuracy(self):
-      predictions = np.array([
-          [0.1, 0.3, 0.5, 0.1],
-          [0.4, 0.3, 0.1, 0.2],
-          [0.2, 0.4, 0.0, 0.4],
-          [0.0, 0.4, 0.0, 0.6],
-          [0.1, 0.7, 0.1, 0.1]
-      ])
-      targets = np.array([
-          [0, 0, 1, 0],
-          [0, 1, 0, 0],
-          [0, 0, 0, 1],
-          [0, 0, 1, 0],
-          [0, 1, 0, 0]
-      ])
-      acc = accuracy(predictions, targets)
-      self.assertEqual(acc, 3./5)
+    predictions = np.array([
+        [0.1, 0.3, 0.5, 0.1],
+        [0.4, 0.3, 0.1, 0.2],
+        [0.2, 0.4, 0.0, 0.4],
+        [0.0, 0.4, 0.0, 0.6],
+        [0.1, 0.7, 0.1, 0.1]
+    ])
+    targets = np.array([
+        [0, 0, 1, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0]
+    ])
+    acc = accuracy(predictions, targets)
+    self.assertEqual(acc, 3./5)
 
